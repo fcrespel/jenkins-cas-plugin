37e032307ac89129130c21f5537569bccdfede8b
Fabien Crespel <fabien@crespel.net>
2012-09-07 00:49:34 +0200

    Add CAS 2.0 Proxy Tickets support and proxy chain validation.

--- src/main/java/org/jenkinsci/plugins/cas/protocols/Cas20Protocol.java
+++ src/main/java/org/jenkinsci/plugins/cas/protocols/Cas20Protocol.java
@@ -4,7 +4,13 @@ import hudson.Extension;
 import hudson.Util;
 import hudson.model.Descriptor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.jasig.cas.client.validation.Cas20ProxyTicketValidator;
 import org.jasig.cas.client.validation.Cas20ServiceTicketValidator;
+import org.jasig.cas.client.validation.ProxyList;
 import org.jasig.cas.client.validation.TicketValidator;
 import org.jenkinsci.plugins.cas.CasProtocol;
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -15,17 +21,41 @@ import org.kohsuke.stapler.DataBoundConstructor;
  * @author Fabien Crespel <fabien@crespel.net>
  */
 public class Cas20Protocol extends CasProtocol {
+	
+	public final Boolean proxyEnabled;
+	public final Boolean proxyAllowAny;
+	public final String proxyAllowList;
+	
+	@Deprecated
+	public Cas20Protocol(String authoritiesAttribute, String fullNameAttribute, String emailAttribute) {
+		this(authoritiesAttribute, fullNameAttribute, emailAttribute, false, true, null);
+	}
 
 	@DataBoundConstructor
-	public Cas20Protocol(String authoritiesAttribute, String fullNameAttribute, String emailAttribute) {
+	public Cas20Protocol(String authoritiesAttribute, String fullNameAttribute, String emailAttribute, Boolean proxyEnabled, Boolean proxyAllowAny, String proxyAllowList) {
 		this.authoritiesAttribute = Util.fixEmptyAndTrim(authoritiesAttribute);
 		this.fullNameAttribute = Util.fixEmptyAndTrim(fullNameAttribute);
 		this.emailAttribute = Util.fixEmptyAndTrim(emailAttribute);
+		this.proxyEnabled = proxyEnabled;
+		this.proxyAllowAny = proxyAllowAny;
+		this.proxyAllowList = proxyAllowList;
 	}
 
 	@Override
 	public TicketValidator createTicketValidator(String casServerUrl) {
-		return new Cas20ServiceTicketValidator(casServerUrl);
+		if (this.proxyEnabled != null && this.proxyEnabled) {
+			Cas20ProxyTicketValidator ptv = new Cas20ProxyTicketValidator(casServerUrl);
+			ptv.setAcceptAnyProxy(this.proxyAllowAny);
+			String[] proxyChain = StringUtils.split(this.proxyAllowList, '\n');
+			if (proxyChain != null && proxyChain.length > 0) {
+				List<String[]> proxyList = new ArrayList<String[]>(1);
+				proxyList.add(proxyChain);
+				ptv.setAllowedProxyChains(new ProxyList(proxyList));
+			}
+			return ptv;
+		} else {
+			return new Cas20ServiceTicketValidator(casServerUrl);
+		}
 	}
 
 	@Extension
